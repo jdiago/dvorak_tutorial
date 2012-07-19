d9306e40a603bf3a2499d0b6f01882e89bdb7ed4
Josef Diago <josef.diago@gmail.com>
2012-07-18 20:23:48 -0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvoraktutorial::Application.config.secret_token = 'feca121cc723ecce4af71f7ffff623b4f017cd795e00cfaacb4b6d06062d8fc242133248691b0cdf58dab488087285ce719cef3840171c0fc6d708e160ff1a38'
