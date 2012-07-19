edf8b9b060cecc852b9fb9f87c38e9d44f8d4060
Josef Diago <josef.diago@gmail.com>
2012-07-19 17:19:54 -0400

    twitter bootstrap

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,18 +8,18 @@ class LessonsController < ApplicationController
   end
 
   def check
-    @lesson = Lesson.find(params[:id])
+    lesson = Lesson.find(params[:id])
 
-    if @lesson.content == params[:answer]
+    if lesson.content == params[:answer]
       begin
-        nextLesson = Lesson.find(@lesson.id + 1)
+        nextLesson = Lesson.find(lesson.id + 1)
       rescue ActiveRecord::RecordNotFound
         redirect_to action: 'index'
       else
         redirect_to action: 'lesson', id: nextLesson.id
       end
     else
-      redirect_to action: 'lesson', id: @lesson.id
+      redirect_to action: 'lesson', id: lesson.id
     end
   end
 end
