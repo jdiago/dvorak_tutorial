08d2cb670f173155fbfc492c60e413b5811067ca
Josef Diago <josef.diago@gmail.com>
2012-07-24 17:47:45 -0400

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_name(params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      
+    end
   end
 
   def destroy
