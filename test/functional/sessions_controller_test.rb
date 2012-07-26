562626443b77097102c170440e6c47844a3af418
Josef Diago <josef.diago@gmail.com>
2012-07-26 19:18:58 -0400

    login tests

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -7,6 +7,20 @@ class SessionsControllerTest < ActionController::TestCase
   end
 
   test "should login" do
-    
+    jo = users(:jo)
+    post :create, name: jo.name, password: 'hunter2'
+    assert_redirected_to root_url
+    assert_equal jo.id, session[:user_id]
+  end
+
+  test "should fail login" do
+    jo = users(:jo)
+    post :create, name: jo.name, password: 'wrong'
+    assert_redirected_to login_url
+  end
+
+  test "should logout" do
+    delete :destroy
+    assert_redirected_to root_url
   end
 end
